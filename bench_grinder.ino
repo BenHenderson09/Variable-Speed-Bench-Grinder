fc75e1ff1992fa7cdac743acd52da3fb915da5d2
ben <benh090704@gmail.com>
2021-02-15 10:56:03 +0000

    Removed unnecessary startup process

--- bench_grinder.ino
+++ bench_grinder.ino
@@ -1,87 +1,31 @@
-#if FALSE
-#include "constants/MotorConstants.hpp"
-
-const int motorControlPin{5};
-const int potentiometerPin{A0};
-
-void setMotorDutyCycle(double dutyCycle, int frequencyInHertz);
-void startMotor();
-
-void setup(){
-    pinMode(motorControlPin, OUTPUT);
-    startMotor();
-
-    Serial.begin(9600);
-}
-
-void loop(){
-    double dutyCycle{((1023-analogRead(potentiometerPin)) / 1023.f) * (1 - MotorConstants::minimumDutyCyclePostStartup) + MotorConstants::minimumDutyCyclePostStartup};
-
-    Serial.println(analogRead(potentiometerPin)); delay(1000);
-    //setMotorDutyCycle(dutyCycle, 1000);
-}
-
-void startMotor(){
-    unsigned long startTime{millis()};
-
-    while (millis() - startTime  <= MotorConstants::startupTimeInMilliseconds){
-        setMotorDutyCycle(MotorConstants::minimumDutyCycleOnStartup, 1000);
-    }
-}
-
-void setMotorDutyCycle(double dutyCycle, int frequencyInHertz){
-    double periodInMicroseconds{10E6 * (1.f / frequencyInHertz)};
-    double highTimeInMicroseconds{dutyCycle * periodInMicroseconds};
-    double lowTimeInMicroseconds{periodInMicroseconds - highTimeInMicroseconds};
-
-    digitalWrite(motorControlPin, HIGH);
-    delayMicroseconds(highTimeInMicroseconds);
-
-    if (lowTimeInMicroseconds != 0){
-      digitalWrite(motorControlPin, LOW);
-      delayMicroseconds(lowTimeInMicroseconds);
-    }
-}
-#else
-
 #include "constants/MotorConstants.hpp"
 #include "constants/PinConstants.hpp"
 
-void startMotor();
 void setMotorDutyCycle(double dutyCycle);
-double getClockwisePotentiometerSettingAsPercentage();
+double getPotentiometerSettingAsPercentage();
 double mapPercentageAboveLowerLimit(double percentage, double lowerLimit);
 
 void setup(){
-    delay(400); // Wait for other components to get ready
     pinMode(PinConstants::motorControlPin, OUTPUT);
-    startMotor();
 }
 
 void loop(){
     double dutyCycle{
         mapPercentageAboveLowerLimit(
-            getClockwisePotentiometerSettingAsPercentage(),
-            MotorConstants::minimumDutyCyclePostStartup
+            getPotentiometerSettingAsPercentage(),
+            MotorConstants::minimumDutyCycle
         )
     };
-    
+
     setMotorDutyCycle(dutyCycle);
 }
 
-void startMotor(){
-    unsigned long startTime{millis()};
-
-    while (millis() - startTime  <= MotorConstants::startupTimeInMilliseconds){
-        setMotorDutyCycle(MotorConstants::minimumDutyCycleOnStartup);
-    }
-}
-
-
-double getClockwisePotentiometerSettingAsPercentage(){
-    int anticlockwisePotentiometerSetting{analogRead(PinConstants::potentiometerPin)};
-    int clockwisePotentiometerSetting{1023 - analogRead(PinConstants::potentiometerPin)};
-    return clockwisePotentiometerSetting / 1023.f;
+double getPotentiometerSettingAsPercentage(){
+    // Range is from 0 to 1023
+    int potentiometerSettingFromRange{analogRead(PinConstants::potentiometerPin)};
+    
+    // Return percentage as a decimal, range 0 to 1
+    return potentiometerSettingFromRange / 1023.f;
 }
 
 double mapPercentageAboveLowerLimit(double percentage, double lowerLimit){
@@ -98,11 +42,10 @@ void setMotorDutyCycle(double dutyCycle){
     delayMicroseconds(highTimeInMicroseconds);
 
     // Delaying by a small number of microseconds can take longer than intended,
-    // so this will run the motor at max speed when the pot is very close to fully turned 
+    // so this check will run the motor without any delay if the potentiometer
+    // is very close to fully turned.
     if (lowTimeInMicroseconds > 1){
       digitalWrite(PinConstants::motorControlPin, LOW);
       delayMicroseconds(lowTimeInMicroseconds);
     }
 }
-
-#endif
--- constants/MotorConstants.hpp
+++ constants/MotorConstants.hpp
@@ -2,9 +2,7 @@
 #define MOTOR_CONSTANTS_HPP
 
 namespace MotorConstants {
-    const int startupTimeInMilliseconds{2000};
-    const double minimumDutyCycleOnStartup{0.4};
-    const double minimumDutyCyclePostStartup{0.18};
+    const double minimumDutyCycle{0.2};
     const int pwmFrequencyInHertz{1000};
 }
 
